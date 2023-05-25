60f6e3c9618204a22f9c72965cc3d368add1d794
diegoirreno <dfirreno@uniboyaca.edu.co>
2023-05-25 11:29:57 -0500

    Se soluciono el error de modificar en la interfaz de estudiante

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require 'models/estudiante.php';
-require 'controllers/conexionBdController.php';
+require 'controllers/conexionDbController.php';
 require 'controllers/baseController.php';
 require 'controllers/estudiantesController.php';
 
@@ -21,13 +21,12 @@ $estudiantes = $estudianteController->read();
 <body>
     <main>
         <h1>Lista de usuarios</h1>
-        <a href="views/form_estudiante.php">Registrar usuario</a>
         <table>
             <thead>
                 <tr>
                     <th>Codigo</th>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
                 </tr>
             </thead>
             <tbody>
@@ -39,14 +38,16 @@ $estudiantes = $estudianteController->read();
                     echo '  <td>' . $estudiante->getApellido() . '</td>';
                     echo '  <td>';
                     echo '      <a href="views/form_estudiante.php?codigo=' . $estudiante->getCodigo() . '">Modificar</a>';
-                    echo '      <a href="views/accion_borrar_usuario.php?codigo=' . $estudiante->getCodigo() . '">Borrar</a>';
-                    echo '      <a href="actividades.php?codigo=' . $estudiante->getCodigo() . '&nombre=' . $estudiante-> getNombre() . '&apellido=' . $estudiante-> getApellido() . '">Notas</a>';
+                    echo '      <a href="views/accion_borrar_estudiante.php?codigo=' . $estudiante->getCodigo() . '">Borrar</a>';
+                    echo '      <a href="models/actividades.php?codigo=' . $estudiante->getCodigo() . '&nombre=' . $estudiante-> getNombre() . '&apellido=' . $estudiante-> getApellido() . '">Notas</a>';
                     echo '  </td>';
                     echo '</tr>';
                 }
                 ?>
             </tbody>
         </table>
+        <br>
+        <a href="views/form_estudiante.php">Registrar usuario</a>
     </main>
 </body>
 
--- views/accion_registrar_estudiante.php
+++ views/accion_registrar_estudiante.php
@@ -1,6 +1,6 @@
 <?php
 require '../models/estudiante.php';
-require '../controllers/conexionBdController.php';
+require '../controllers/conexionDbController.php';
 require '../controllers/baseController.php';
 require '../controllers/estudiantesController.php';
 
@@ -9,13 +9,16 @@ use estudianteController\EstudianteController;
 
 $estudiante = new Estudiante();
 $estudiante->setCodigo($_POST['codigo']);
-$estudiante->setNombre($_POST['nombres']);
-$usuario->setApellido($_POST['apellidos']);
+$estudiante->setNombre($_POST['nombre']);
+$estudiante->setApellido($_POST['apellido']);
 
 $estudianteController = new EstudianteController();
-$resultado = $usuarioController->create($estudiante);
+$resultado = $estudianteController->create($estudiante);
 if ($resultado) {
     echo '<h1>Estudiante registrado</h1>';
 } else {
     echo '<h1>No se pudo registrar al estudiante</h1>';
 }
+?>
+<br>
+<a href="../index.php">Volver al inicio</a>
