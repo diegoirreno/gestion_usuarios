8807bde1e21c6a3c8e0b858630b1cf118c532b55
diegoirreno <dfirreno@uniboyaca.edu.co>
2023-05-27 00:19:13 -0500

    Ocurrio un problema para seguir comentariando el proyecto , solucionado se modifico y reparo la tabla de actividades

--- actividades.php
+++ actividades.php
@@ -1,55 +1,93 @@
 <?php
-require 'models/actividad.php';
-require 'controllers/conexionDbController.php';
-require 'controllers/baseController.php';
-require 'controllers/actividadesController.php';
 
-use actividadController\ActividadController;
+    require 'models/estudiante.php';
+    require 'models/actividad.php';
+    require 'controllers/conexionDbController.php';
+    require 'controllers/baseController.php';
+    require 'controllers/actividadesController.php';
 
-$actividadController = new ActividadController();
+    use estudiante\Estudiante;
+    use actividad\Actividad;
+    use actividadController\ActividadController;
 
-$actividades = $actividadController->read($codigo);
+    $codigoEstudiante = $_GET['codigo'];
+    $nombreEstudiante = $_GET['nombre'];
+    $apellidoEstudiante = $_GET['apellido'];
+    
+    $actividadController = new ActividadController();
+    $actividades = $actividadController->read($codigoEstudiante);
+
 ?>
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividades</title>
+    <link rel="stylesheet" href="views/css/estilos.css">
 </head>
 
 <body>
-    <main>
-        <h1>Lista de actividades</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Descripcion</th>
-                    <th>Nota</th>
-                    <th>Codigo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                foreach ($actividades as $actividad) {
-                    echo '<tr>';
-                    echo '  <td>' . $actividad->getId() . '</td>';
-                    echo '  <td>' . $actividad->getDescripcion() . '</td>';
-                    echo '  <td>' . $actividad->getNota() . '</td>';
-                    echo '  <td>' . $actividad->getCodigoE() . '</td>';
-                    echo '  <td>';
-                    echo '      <a href="views/form_actividad.php?id=' . $actividad->getId() . '">Modificar</a>';
-                    echo '      <a href="views/accion_borrar_actividad.php?id=' . $actividad->getId() . '">Borrar</a>';
-                    echo '  </td>';
-                    echo '</tr>';
-                }
-                ?>
-            </tbody>
-        </table>
-        <br>
-        <a href="views/form_actividad.php">Registrar Actividad</a>
-    </main>
+    
 </body>
+
+    <main>
+            <h1>Actividades</h1>
+
+            <?php
+            
+            echo '<h3>Estudiante: ' . $nombreEstudiante . '</h3>';
+            echo '<h3>Código: ' . $codigoEstudiante . '</h3>';
+            
+            ?>
+
+            <br>
+
+            <table >
+                <thead>
+                    <tr class="table-encabezado">
+                        <th>ID</th>
+                        <th>Actividad</th>
+                        <th>Nota</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach($actividades as $actividad){
+                        echo '<tr>';
+                        echo '<td>' . $actividad->getId() . '</td>';
+                        echo '<td>' . $actividad->getDescripcion() . '</td>';
+                        echo '<td>' . $actividad->getNota() . '</td>';
+                        echo '<td>';
+                        echo '      <a href="views/form_actividad.php?id=' . $actividad->getId() . ' &codigo=' . $codigoEstudiante . '&nombre=' . $nombreEstudiante . '&apellido=' . $apellidoEstudiante . '">Modificar</a>';
+                        echo '      <a href="views/accion_borrar_actividad.php?id=' . $actividad->getId() . '&codigo=' . $codigoEstudiante . '&nombre=' . $nombreEstudiante . '&apellido=' . $apellidoEstudiante . '">Eliminar</a>';
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+
+                    ?>
+                </tbody>
+            </table>
+            <br>
+            <?php
+            ?>
+            <?php
+
+            echo '<form action="views/form_actividad.php" method="post">';
+            echo '<input type="hidden" name="codigo" value="' . $codigoEstudiante . '">';
+            echo '<input type="hidden" name="nombre" value="' . $nombreEstudiante . '">';
+            echo '<input type="hidden" name="apellido" value="' . $apellidoEstudiante . '">';
+            echo '<button type="submit">Agregar actividad</button>';
+            echo '</form>';
+
+            ?>
+
+
+            <a href="index.php">Volver</a>
+        </main>
 
 </html>
